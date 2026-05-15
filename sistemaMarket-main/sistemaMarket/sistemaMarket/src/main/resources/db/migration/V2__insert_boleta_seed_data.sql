edf60a58e429e4157ba0bb707dd028d95f7975b8
Kenneth-Araya <karayaavalos@gmail.com>
2026-05-15 02:37:07 -0400

    se arreglan problemas en la query de boleta

--- sistemaMarket-main/sistemaMarket/sistemaMarket/src/main/resources/db/migration/V2__insert_boleta_seed_data.sql
+++ sistemaMarket-main/sistemaMarket/sistemaMarket/src/main/resources/db/migration/V2__insert_boleta_seed_data.sql
@@ -1,7 +1,8 @@
 INSERT INTO bd_boletas.boleta
-(folio, rut_proveedor, glosa, tipo, fecha_emision, monto_bruto, monto_neto, estado)
+(folio_boleta, glosa_boleta, tipo_boleta, fecha_emision_boleta, monto_bruto_boleta, monto_neto_boleta, estado_boleta)
 VALUES
-('BLT-1001','76123456-7','Compra de insumos de oficina','AFECTA','2026-03-01',119000,100000,'EMITIDA'),
-('BLT-1002','77333444-5','Servicio de mantención de equipos','AFECTA','2026-03-03',238000,200000,'PAGADA'),
-('BLT-1003','78444555-6','Adquisición de materiales eléctricos','AFECTA','2026-03-05',59500,50000,'PENDIENTE'),
-('BLT-1010','75222333-5','Compra de útiles escolares','EXENTA','2026-03-18',38000,38000,'PENDIENTE');
+('BLT-1001','Compra de insumos de oficina','AFECTA','2026-03-01',119000,100000,'EMITIDA'),
+('BLT-1002','Servicio de mantención de equipos','AFECTA','2026-03-03',238000,200000,'PAGADA'),
+('BLT-1003','Adquisición de materiales eléctricos','AFECTA','2026-03-05',59500,50000,'PENDIENTE'),
+('BLT-1010','Compra de útiles escolares','EXENTA','2026-03-18',38000,38000,'PENDIENTE');
+
